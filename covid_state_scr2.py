b855d14ea27e014c6fb3a033059ec781faed64a3
MeetPancholi01 <85289669+MeetPancholi01@users.noreply.github.com>
2022-01-10 22:40:33 +0530

    Update covid_state_scr2.py

--- covid_state_scr2.py
+++ covid_state_scr2.py
@@ -1,14 +1,19 @@
 from selenium import webdriver
 import re
+import os
 def covid_state(state='Gujarat'):
     state = state.lower()
     state = state + ' '
 
     options = webdriver.ChromeOptions()
+    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     options.add_argument('--ignore-certificate-errors-spki-list')
     options.add_argument('--ignore-ssl-errors')
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-sh-usage')
 
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"),chrome_options=options)
 
     driver.get('https://covid19tracker.in/')
 
